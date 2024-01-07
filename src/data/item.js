a545da5243ca5b45b19fa140caf970cb5f435694
pankajsaini <pankajsaini2762@gmail.com>
2024-01-07 19:22:30 +0530

    first commit

---
+++ src/data/item.js
@@ -0,0 +1,113 @@
+const DEFAULT_ITEMS = [
+  {
+      id:'001',
+      image:'Images/1.webp',
+      company:'Cartloon London',
+      item_name:'Women Solid Joggers',
+      original_price:1085,
+      current_price:503,
+      discount_percentage:45,
+      return_period:14,
+      delivery_date:'10 oct 2023',
+      rating:{
+          stars:4.5,
+          count:1400
+      },
+
+
+  },
+  {
+      id:'002',
+      image:'Images/01.webp',
+      company:'Singni',
+      item_name:'Women Embroided Kurta Set',
+      original_price:5499,
+      current_price:989,
+      discount_percentage:82,
+      return_period:14,
+      delivery_date:'10 oct 2023',
+      rating:{
+          stars:4.1,
+          count:1580
+      },
+
+
+  },
+  {
+      id:'003',
+      image:'Images/002.webp',
+      company:'H&M',
+      item_name:'Regular Fit V-neck T-shirt',
+      original_price:499,
+      current_price:274,
+      discount_percentage:45,
+      return_period:14,
+      delivery_date:'10 oct 2023',
+      rating:{
+          stars:4.5,
+          count:163
+      },
+
+
+  },
+  {
+      id:'004',
+      image:'Images/03.webp',
+      company:'Puma',
+      item_name:'Regular Fit V-neck T-shirt',
+      original_price:4999,
+      current_price:2999,
+      discount_percentage:40,
+      return_period:14,
+      delivery_date:'10 oct 2023',
+      rating:{
+          stars:1.3,
+          count:3
+      },
+
+
+  },
+  {
+      id:'005',
+      image:'Images/04.webp',
+      company:'Aarika',
+      item_name:'Girls Padded Jacket',
+      original_price:1879,
+      current_price:599,
+      discount_percentage:1280,
+      return_period:14,
+      delivery_date:'10 oct 2023',
+      rating:{
+          stars:1.3,
+          count:5
+      },
+
+
+  },
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
